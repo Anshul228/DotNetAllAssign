32c97a469be21995f7544cdef4911e14b0a16f12
Anshul228 <68274726+Anshul228@users.noreply.github.com>
2020-08-12 09:42:13 +0530

    Add files via upload

---
+++ WindowsFormsCal_Xml/WindowsFormsCal_Xml/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsCal_Xml
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Calculator = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.add = new System.Windows.Forms.Button();
+            this.sub = new System.Windows.Forms.Button();
+            this.Div = new System.Windows.Forms.Button();
+            this.Mul = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Calculator
+            // 
+            this.Calculator.AutoSize = true;
+            this.Calculator.Location = new System.Drawing.Point(280, 27);
+            this.Calculator.Name = "Calculator";
+            this.Calculator.Size = new System.Drawing.Size(71, 17);
+            this.Calculator.TabIndex = 0;
+            this.Calculator.Text = "Calculator";
+            this.Calculator.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter First number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(71, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Second Number ";
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(283, 87);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 22);
+            this.txt1.TabIndex = 3;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(283, 142);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 22);
+            this.txt2.TabIndex = 4;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(71, 258);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 5;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // sub
+            // 
+            this.sub.Location = new System.Drawing.Point(246, 257);
+            this.sub.Name = "sub";
+            this.sub.Size = new System.Drawing.Size(75, 23);
+            this.sub.TabIndex = 6;
+            this.sub.Text = "Sub";
+            this.sub.UseVisualStyleBackColor = true;
+            this.sub.Click += new System.EventHandler(this.sub_Click);
+            // 
+            // Div
+            // 
+            this.Div.Location = new System.Drawing.Point(431, 256);
+            this.Div.Name = "Div";
+            this.Div.Size = new System.Drawing.Size(75, 23);
+            this.Div.TabIndex = 7;
+            this.Div.Text = "Div";
+            this.Div.UseVisualStyleBackColor = true;
+            this.Div.Click += new System.EventHandler(this.Div_Click);
+            // 
+            // Mul
+            // 
+            this.Mul.Location = new System.Drawing.Point(608, 257);
+            this.Mul.Name = "Mul";
+            this.Mul.Size = new System.Drawing.Size(75, 23);
+            this.Mul.TabIndex = 8;
+            this.Mul.Text = "Mul";
+            this.Mul.UseVisualStyleBackColor = true;
+            this.Mul.Click += new System.EventHandler(this.Mul_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(74, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(246, 317);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 17);
+            this.label4.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(431, 317);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(608, 317);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 17);
+            this.label6.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Mul);
+            this.Controls.Add(this.Div);
+            this.Controls.Add(this.sub);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Calculator);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Calculator;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button sub;
+        private System.Windows.Forms.Button Div;
+        private System.Windows.Forms.Button Mul;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsCal_Xml/WindowsFormsCal_Xml/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassCalculate;
+
+namespace WindowsFormsCal_Xml
+{
+    public partial class Form1 : Form
+    {
+        Class1 c = new Class1();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            c.x = Convert.ToInt32(txt1.Text);
+            c.y = Convert.ToInt32(txt2.Text);
+            label3.Text = c.Add().ToString();
+        }
+
+        private void sub_Click(object sender, EventArgs e)
+        {
+            c.x = Convert.ToInt32(txt1.Text);
+            c.y = Convert.ToInt32(txt2.Text);
+            label4.Text = c.Sub().ToString();
+        }
+
+        private void Div_Click(object sender, EventArgs e)
+        {
+            c.x = Convert.ToInt32(txt1.Text);
+            c.y = Convert.ToInt32(txt2.Text);
+            label5.Text = c.Div().ToString();
+        }
+
+        private void Mul_Click(object sender, EventArgs e)
+        {
+            c.x = Convert.ToInt32(txt1.Text);
+            c.y = Convert.ToInt32(txt2.Text);
+            label6.Text = c.Mult().ToString();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
